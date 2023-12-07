5bfcd358ed3a7d390e53ac9c8be1fcef384838ca
772178+iamjustsam@users.noreply.github.com <772178+iamjustsam@users.noreply.github.com>
2023-12-07 23:48:55 +0100

    Day 1 + 2

---
+++ Day1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var numberRegex = new Regex("one|two|three|four|five|six|seven|eight|nine");
+var rtlNumberRegex = new Regex("one|two|three|four|five|six|seven|eight|nine", RegexOptions.RightToLeft);
+var numbers = new Dictionary<string, int>
+{
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
+};
+var input = File.ReadAllLines("input.txt");
+var output = input.Select(x =>
+{
+    var firstNumber = numberRegex.Replace(x, m => $"{numbers[m.Value]}", 1);
+    var lastNumber = rtlNumberRegex.Replace(x, m => $"{numbers[m.Value]}", 1);
+
+    return int.Parse($"{firstNumber.First(char.IsDigit)}{lastNumber.Last(char.IsDigit)}");
+}).Sum();
+
+Console.WriteLine(output);
---
+++ Day2/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+var gameIdRegex = new Regex("^Game (\\d+):.*$");
+var redCubesRegex = new Regex("(\\d+) red");
+var greenCubesRegex = new Regex("(\\d+) green");
+var blueCubesRegex = new Regex("(\\d+) blue");
+
+const int maxRed = 12;
+const int maxGreen = 13;
+const int maxBlue = 14;
+
+var input = File.ReadLines("input.txt");
+
+var games = input.Select(x =>
+{
+    var gameId = int.Parse(gameIdRegex.Match(x).Groups[1].Value);
+    var gameSets = x.Split(';').Select(gameSetInput =>
+    {
+        var redCubeCount = redCubesRegex.Match(gameSetInput).Groups[1].Value;
+        var greenCubeCount = greenCubesRegex.Match(gameSetInput).Groups[1].Value;
+        var blueCubeCount = blueCubesRegex.Match(gameSetInput).Groups[1].Value;
+
+        return new GameSet(ParseInt(redCubeCount), ParseInt(greenCubeCount), ParseInt(blueCubeCount));
+    }).ToArray();
+
+    return new GameInfo(gameId, gameSets);
+}).ToArray();
+
+var possibleGames = games.Where(x => x.Sets.Max(y => y.RedCount) <= maxRed && x.Sets.Max(y => y.GreenCount) <= maxGreen && x.Sets.Max(y => y.BlueCount) <= maxBlue);
+var output1 = possibleGames.Sum(x => x.Id);
+
+var minimumSetPowers = games.Select(x =>
+{
+    var minRed = x.Sets.Max(y => y.RedCount);
+    var minGreen = x.Sets.Max(y => y.GreenCount);
+    var minBlue = x.Sets.Max(y => y.BlueCount);
+
+    return minRed * minGreen * minBlue;
+});
+var output2 = minimumSetPowers.Sum();
+
+Console.WriteLine($"Sum of possible games: {output1}");
+Console.WriteLine($"Sum of powers: {output2}");
+
+return;
+
+static int ParseInt(string input) => string.IsNullOrWhiteSpace(input) ? 0 : int.Parse(input);
+
+record GameInfo(int Id, GameSet[] Sets);
+record GameSet(int RedCount, int GreenCount, int BlueCount);
